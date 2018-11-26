ab07cab3a7093ca3c7cb5cb224f31a7f11b0fe40
olinka-1993 <olya.yeritspokhyan@gmail.com>
2018-11-26 21:17:15 +0400

    modified the query for bus routes

--- api/routes/busRoutes.js
+++ api/routes/busRoutes.js
@@ -6,18 +6,28 @@ const dbClient = require('./../../database')
 
 
 async function selectBusIds(from,to){
-const text = 'SELECT a.bus_id FROM bus_routes AS a JOIN bus_routes AS b ON a.bus_id=b.bus_id WHERE a.street_name=$1 AND b.street_name=$2;'
+// const text = 'SELECT a.bus_id FROM bus_routes AS a JOIN bus_routes AS b ON a.bus_id=b.bus_id WHERE a.street_name=$1 AND b.street_name=$2;'
+const text = 'SELECT * from bus_routes where bus_id in (SELECT a.bus_id FROM bus_routes AS a JOIN bus_routes AS b ON  a.bus_id=b.bus_id WHERE a.street_name=$1 AND b.street_name=$2) and route_order between (select route_order from bus_routes where street_name = $1) and (select route_order from bus_routes where street_name = $2);'
 const values = [from, to]
 return  await dbClient.query(text,values)
 }
     // const values = ["Erebuni", 'Khanjyan']
 
     router.get('/',async (req,res,next)=>{
-        const query = await selectBusIds(req.query.from, req.query.to)
-        const result = query.rows[0]
-        res.status(200).json({
-        result
-    })
+        try{
+            const query = await selectBusIds(req.query.from, req.query.to)
+            console.log("result",query)
+            const result = query.rows
+            let mod = {}
+            for(line in query.rows){
+                
+            }
+            res.status(200).json({
+            result
+        })
+        }catch(err){
+console.log(err)
+        }
     })
 
 // router.get('/',async (req,res,next)=>{
